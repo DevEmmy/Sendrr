c5bd04e8033a6554507c11da0d688d8c4a24a565
web-tranquility <eolaosebikan60@gmail.com>
2024-06-28 09:58:28 +0100

    added hero

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Nav from "../Nav"
 import { MdOutlineArrowOutward } from 'react-icons/md'
+import Hero from './Hero'
 
 const LandingPage = () => {
   return (
     <div>
         <Nav />
 
-        
+        <Hero />
     </div>
   )
 }
